6cfc3eeb30f8f8af564df482e6c161167bbcf848
Ronald <ronald@mbp-de-ronald.home>
2022-11-15 20:06:12 +0100

    fix issue on insertData

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -38,16 +38,17 @@ const mainController = {
         },
     // Gestion insertion d'un nouvel élève    
     async InsertData(req,res){
+        const promoSearched = req.query.promoSearched;
         console.log(req.body.prenom)
         //const promoSearched = req.query.promoSearched;
         const dataToInsert = `INSERT INTO students (first_name,last_name,promo) VALUES ('${req.body.prenom}','${req.body.nom}',${req.body.promo});`;
-        const result = await client.query(dataToInsert)
+        await client.query(dataToInsert)
         try {
-            console.log(result.rowCount);
-            res.redirect('/promo/:promo');
+            res.redirect(`/search?promoSearched=${req.query.promoSearched}`);
+            //console.log(result);
         }
         catch(error){
-            console.log(err.stack)
+            console.log(error.stack)
         }
     }
          
