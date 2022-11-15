5ea00f879c49a66b48290ea18ccd18ca376159e6
Ronald <ronald@mbp-de-ronald.home>
2022-11-15 19:23:21 +0100

    conversion terminé en async await sur mainController

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,7 +1,7 @@
 const client = require('../data/dataOclock.js')
 
 const mainController = {
-
+    // Afficher l'ensemble de mes promos
      async displayPromos(req,res){
         const findPromo = 'SELECT * FROM promo;';
         const result = await client.query(findPromo)
@@ -9,8 +9,8 @@ const mainController = {
             res.render('index');}          
         catch(err) {console.log(err.stack)};
                 },
+     // Je récupère la liste des students de la promo en cours de visualisation
      async displayStudents(req,res){
-        // Je récupère la liste des students de la promo en cours de visualisation
         const students = `SELECT * FROM students WHERE promo=${req.params.promo};`;
         const result = await client.query(students)
         try {res.locals.studentsResult = result.rows;
@@ -18,60 +18,37 @@ const mainController = {
         }
         catch (error) {console.log(error.stack)}
      },
-     displaySearch(req,res){
+     // Gestion de la recherche d'une promo
+     async displaySearch(req,res){
         // Gestion de la recherche de la promo
         const promoSearched = req.query.promoSearched;
-
-        console.log(promoSearched);
         const promoSearchedResult = `SELECT * FROM students WHERE promo=${promoSearched};`;
         if (promoSearched) {
-            client
-            .query(promoSearchedResult)
-            .then(result => {
-                // controle de l'existance de la promo, sinon 404 error
-                if (result.rowCount == 0) {
-                    res.render('404')
-                }
+            const result = await client.query(promoSearchedResult)
+            // si la promo n'existe pas
+            try {if (result.rowCount == 0) {
+                res.render('404')}
                 res.locals.studentsResult = result.rows;
                 res.render('students');
                 console.log(result.rowCount);
-            })
-            .catch(err => console.log(err.stack));
-        } else {
-            res.render('404')
+            }
+            catch (error) { console.log(error.stack);res.render('404')}
         }
-        
-    },
-    InsertData(req,res){
-console.log(req.body.prenom)
-const promoSearched = req.query.promoSearched;
-const dataToInsert = `INSERT INTO students (first_name,last_name,promo) VALUES ('${req.body.prenom}','${req.body.nom}',${req.body.promo});`;
-client
-.query(dataToInsert)
-.then(result => {
-
-    console.log(result.rowCount);
-        res.redirect('/promo/:promo')
-})
-.catch(err => console.log(err.stack))
-
-        //console.log(promoSearched);
-        // const promoSearchedResult = `SELECT * FROM students WHERE promo=${promoSearched};`;
-        // if (promoSearched) {
-        //     client
-        //     .query(promoSearchedResult)
-        //     .then(result => {
-        //         // controle de l'existance de la promo, sinon 404 error
-        //         if (result.rowCount == 0) {
-        //             res.render('404')
-        //         }
-        //         res.locals.studentsResult = result.rows;
-        //         res.render('students');
-        //     })
-        //     .catch(err => console.log(err.stack));
-        // } else {
-        //     res.render('404')
-        // }
+            
+        },
+    // Gestion insertion d'un nouvel élève    
+    async InsertData(req,res){
+        console.log(req.body.prenom)
+        //const promoSearched = req.query.promoSearched;
+        const dataToInsert = `INSERT INTO students (first_name,last_name,promo) VALUES ('${req.body.prenom}','${req.body.nom}',${req.body.promo});`;
+        const result = await client.query(dataToInsert)
+        try {
+            console.log(result.rowCount);
+            res.redirect('/promo/:promo');
+        }
+        catch(error){
+            console.log(err.stack)
+        }
     }
          
     }
